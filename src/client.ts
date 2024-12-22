9fc5ccdd778ae02df3c374ddaba1a7cc742567dd
LakshmanKishore <lakshmankishore17@gmail.com>
2024-12-22 17:49:30 +0530

    feature: Add click events to place the users avatar on the board

--- src/client.ts
+++ src/client.ts
@@ -1,45 +1,133 @@
 import "./styles.css"
 
-import { PlayerId } from "rune-sdk"
+import { Player, PlayerId } from "rune-sdk"
 
 import selectSoundAudio from "./assets/select.wav"
 import { Cells } from "./logic.ts"
 
 const board = document.getElementById("board")!
 const playersSection = document.getElementById("playersSection")!
+const gameBoardSVG = document.getElementById("game-board-svg")!
+// const buttonsContainer = document.getElementById("buttons-container")!
 
 const selectSound = new Audio(selectSoundAudio)
 
-let cellButtons: HTMLButtonElement[], playerContainers: HTMLLIElement[]
+let cellImages: SVGImageElement[], playerContainers: HTMLLIElement[]
+
+// Define the Constants
+const LINES_COORDINATES = [
+  { x1: 0, y1: 300, x2: 200, y2: 300 },
+  { x1: 300, y1: 0, x2: 300, y2: 200 },
+  { x1: 600, y1: 300, x2: 400, y2: 300 },
+  { x1: 300, y1: 400, x2: 300, y2: 600 },
+]
+
+const RECT_COORDINATES = [
+  { x: 200, y: 200, width: 200, height: 200 },
+  { x: 100, y: 100, width: 400, height: 400 },
+  { x: 0, y: 0, width: 600, height: 600 },
+]
+
+const ELLIPSE_STROKE_WIDTH = (7).toString()
+const LINE_STROKE_WIDTH = (5).toString()
+const CELL_SIZE = (+ELLIPSE_STROKE_WIDTH * 4).toString()
 
 function initUI(
-  cells: Cells,
+  cells: Cells[],
   playerIds: PlayerId[],
   yourPlayerId: PlayerId | undefined
 ) {
-  cellButtons = cells.map((_, cellIndex) => {
-    const button = document.createElement("button")
-    button.addEventListener("click", () => Rune.actions.claimCell(cellIndex))
-    // board.appendChild(button)
+  // Create lines at the linesCoordinates
+  for (let i = 0; i < LINES_COORDINATES.length; i++) {
+    const line = document.createElementNS("http://www.w3.org/2000/svg", "line")
+    line.setAttribute("x1", LINES_COORDINATES[i].x1.toString())
+    line.setAttribute("y1", LINES_COORDINATES[i].y1.toString())
+    line.setAttribute("x2", LINES_COORDINATES[i].x2.toString())
+    line.setAttribute("y2", LINES_COORDINATES[i].y2.toString())
+    line.setAttribute(
+      "style",
+      `stroke: #e6e6e6; stroke-width: ${LINE_STROKE_WIDTH};`
+    )
+    gameBoardSVG.appendChild(line)
+  }
 
-    return button
+  // Create rects at the rectCoordinates
+  for (let i = 0; i < RECT_COORDINATES.length; i++) {
+    const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect")
+    rect.setAttribute("x", RECT_COORDINATES[i].x.toString())
+    rect.setAttribute("y", RECT_COORDINATES[i].y.toString())
+    rect.setAttribute("width", RECT_COORDINATES[i].width.toString())
+    rect.setAttribute("height", RECT_COORDINATES[i].height.toString())
+    rect.setAttribute(
+      "style",
+      `stroke: #e6e6e6; fill: rgba(0, 0, 0, 0); stroke-width: ${LINE_STROKE_WIDTH};`
+    )
+    gameBoardSVG.appendChild(rect)
+  }
+
+  cellImages = cells.map((_, index) => {
+    const ellipse = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "ellipse"
+    )
+    ellipse.setAttribute("cx", cells[index].x.toString())
+    ellipse.setAttribute("cy", cells[index].y.toString())
+    ellipse.setAttribute("rx", ELLIPSE_STROKE_WIDTH)
+    ellipse.setAttribute("ry", ELLIPSE_STROKE_WIDTH)
+    ellipse.setAttribute("style", "stroke: #e6e6e6;")
+
+    // Creating another ellipses for capturing the click
+    const ellipseB = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "ellipse"
+    )
+    ellipseB.setAttribute("cx", cells[index].x.toString())
+    ellipseB.setAttribute("cy", cells[index].y.toString())
+    ellipseB.setAttribute("rx", CELL_SIZE)
+    ellipseB.setAttribute("ry", CELL_SIZE)
+    ellipseB.setAttribute("style", "fill: #ffffff00;")
+
+    // const rectangleIndex = Math.floor(index / 8)
+    // const cellIndexInRectangle = index % 8
+    // Set the index with the rectangular index with the cell index
+    // ellipse.setAttribute(
+    //   "index",
+    //   rectangleIndex.toString() + cellIndexInRectangle.toString()
+    // )
+    ellipseB.addEventListener("click", () => Rune.actions.handleClick(index))
+
+    // Create a svg image element which will then be used to set the image of avatar
+    const image = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "image"
+    )
+
+    image.setAttribute("x", (cells[index].x - +CELL_SIZE).toString())
+    image.setAttribute("y", (cells[index].y - +CELL_SIZE).toString())
+
+    gameBoardSVG.appendChild(ellipse)
+    // Need to add this later for the z-index
+    gameBoardSVG.appendChild(ellipseB)
+    gameBoardSVG.appendChild(image)
+
+    // buttonsContainer.appendChild(button)
+    return image
   })
-
-  // Create a svg element for the game board
-  const svg = document.createElement("svg")
-  svg.classList.add("game-board-svg")
-  board.appendChild(svg)
 
   playerContainers = playerIds.map((playerId, index) => {
     const player = Rune.getPlayerInfo(playerId)
 
     const li = document.createElement("li")
     li.setAttribute("player", index.toString())
-    li.innerHTML = `<img src="${player.avatarUrl}" />
+    li.innerHTML = `<div class="player-info"><img src="${player.avatarUrl}" />
            <span>${
              player.displayName +
              (player.playerId === yourPlayerId ? "<br>(You)" : "")
-           }</span>`
+           }</span></div>`
+    li.innerHTML += `<div class="remaining-mills"><span>xxx</span><span>xxx</span><span>xxx</span></div>`
+    // li.innerHTML += `<span>xxx</span>`
+    // li.innerHTML += `<span>xxx</span></div>`
+    // li.innerHTML += `<div><span>xxx</span></div>`
     playersSection.appendChild(li)
 
     return li
@@ -48,38 +136,57 @@ function initUI(
 
 Rune.initClient({
   onChange: ({ game, yourPlayerId, action }) => {
-    const { cells, playerIds, winCombo, lastMovePlayerId, freeCells } = game
+    const { cells, playerIds, winCombo, lastMovePlayerId } = game
 
-    if (!cellButtons) initUI(cells, playerIds, yourPlayerId)
+    if (!cellImages) initUI(cells, playerIds, yourPlayerId)
 
     if (lastMovePlayerId) board.classList.remove("initial")
+    console.log("Here", lastMovePlayerId)
 
-    cellButtons.forEach((button, i) => {
-      const cellValue = cells[i]
+    // Get all the player ids information
+    const playersInfo = playerIds.reduce(
+      (acc, playerId) => {
+        const info = Rune.getPlayerInfo(playerId)
+        acc[playerId] = info
+        return acc
+      },
+      {} as { [playerId: string]: Player }
+    )
 
-      button.setAttribute(
+    cellImages.forEach((cellImage, i) => {
+      const cellValue: PlayerId | null = cells[i].playerId
+
+      cellImage.setAttribute(
         "player",
         (cellValue !== null ? playerIds.indexOf(cellValue) : -1).toString()
       )
-      button.setAttribute(
-        "dim",
-        String((winCombo && !winCombo.includes(i)) || (!freeCells && !winCombo))
-      )
+
+      // If cell has a player id then have to show player id's avatar
+      if (cellValue) {
+        cellImage.setAttribute("width", (+CELL_SIZE * 2).toString())
+        cellImage.setAttribute("height", (+CELL_SIZE * 2).toString())
+        cellImage.setAttribute("href", playersInfo[cellValue].avatarUrl)
+      }
+
+      // button.setAttribute(
+      //   "dim",
+      //   String((winCombo && !winCombo.includes(i)) || (!freeCells && !winCombo))
+      // )
 
       if (cells[i] || lastMovePlayerId === yourPlayerId || winCombo) {
-        button.setAttribute("disabled", "")
+        cellImage.setAttribute("disabled", "")
       } else {
-        button.removeAttribute("disabled")
+        cellImage.removeAttribute("disabled")
       }
     })
 
     playerContainers.forEach((container, i) => {
       container.setAttribute(
         "your-turn",
-        String(playerIds[i] !== lastMovePlayerId && !winCombo && freeCells)
+        String(playerIds[i] !== lastMovePlayerId)
       )
     })
 
-    if (action && action.name === "claimCell") selectSound.play()
+    if (action && action.name === "handleClick") selectSound.play()
   },
 })
--- src/logic.ts
+++ src/logic.ts
@@ -1,8 +1,15 @@
 import type { PlayerId, RuneClient } from "rune-sdk"
 
-export type Cells = (PlayerId | null)[]
+// export type Cells = PlayerId[]
+export type Cells = {
+  x: number
+  y: number
+  playerId: PlayerId | null
+  reachableCellIndexes: number[]
+}
+
 export interface GameState {
-  cells: Cells
+  cells: Cells[]
   winCombo: number[] | null
   lastMovePlayerId: PlayerId | null
   playerIds: PlayerId[]
@@ -10,72 +17,81 @@ export interface GameState {
 }
 
 type GameActions = {
-  claimCell: (cellIndex: number) => void
+  handleClick: (cellIndex: number) => void
 }
 
 declare global {
   const Rune: RuneClient<GameState, GameActions>
 }
 
-function findWinningCombo(cells: Cells) {
-  return (
-    [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ].find((combo) =>
-      combo.every((i) => cells[i] && cells[i] === cells[combo[0]])
-    ) || null
-  )
-}
+// function findWinningCombo(cells: Cells) {
+//   return (
+//     [
+//       [0, 1, 2],
+//       [3, 4, 5],
+//       [6, 7, 8],
+//       [0, 3, 6],
+//       [1, 4, 7],
+//       [2, 5, 8],
+//       [0, 4, 8],
+//       [2, 4, 6],
+//     ].find((combo) =>
+//       combo.every((i) => cells[i] && cells[i] === cells[combo[0]])
+//     ) || null
+//   )
+// }
 
 Rune.initLogic({
   minPlayers: 2,
   maxPlayers: 2,
   setup: (allPlayerIds) => ({
-    cells: new Array(9).fill(null),
+    cells: [
+      { x: 0, y: 0, playerId: null, reachableCellIndexes: [1, 7] },
+      { x: 300, y: 0, playerId: null, reachableCellIndexes: [0, 2, 9] },
+      { x: 600, y: 0, playerId: null, reachableCellIndexes: [1, 3] },
+      { x: 600, y: 300, playerId: null, reachableCellIndexes: [2, 4, 11] },
+      { x: 600, y: 600, playerId: null, reachableCellIndexes: [3, 5] },
+      { x: 300, y: 600, playerId: null, reachableCellIndexes: [4, 6, 13] },
+      { x: 0, y: 600, playerId: null, reachableCellIndexes: [5, 7] },
+      { x: 0, y: 300, playerId: null, reachableCellIndexes: [0, 6, 15] },
+      { x: 100, y: 100, playerId: null, reachableCellIndexes: [9, 15] },
+      { x: 300, y: 100, playerId: null, reachableCellIndexes: [1, 8, 10, 17] },
+      { x: 500, y: 100, playerId: null, reachableCellIndexes: [9, 11] },
+      { x: 500, y: 300, playerId: null, reachableCellIndexes: [3, 10, 12, 19] },
+      { x: 500, y: 500, playerId: null, reachableCellIndexes: [11, 13] },
+      { x: 300, y: 500, playerId: null, reachableCellIndexes: [5, 12, 14, 21] },
+      { x: 100, y: 500, playerId: null, reachableCellIndexes: [13, 15] },
+      { x: 100, y: 300, playerId: null, reachableCellIndexes: [7, 8, 14, 23] },
+      { x: 200, y: 200, playerId: null, reachableCellIndexes: [17, 23] },
+      { x: 300, y: 200, playerId: null, reachableCellIndexes: [9, 16, 18] },
+      { x: 400, y: 200, playerId: null, reachableCellIndexes: [17, 19] },
+      { x: 400, y: 300, playerId: null, reachableCellIndexes: [18, 11, 20] },
+      { x: 400, y: 400, playerId: null, reachableCellIndexes: [19, 21] },
+      { x: 300, y: 400, playerId: null, reachableCellIndexes: [13, 20, 22] },
+      { x: 200, y: 400, playerId: null, reachableCellIndexes: [21, 23] },
+      { x: 200, y: 300, playerId: null, reachableCellIndexes: [15, 16, 22] },
+    ],
     winCombo: null,
-    lastMovePlayerId: null,
+    lastMovePlayerId: allPlayerIds[1],
     playerIds: allPlayerIds,
   }),
   actions: {
-    claimCell: (cellIndex, { game, playerId, allPlayerIds }) => {
-      if (
-        game.cells[cellIndex] !== null ||
-        playerId === game.lastMovePlayerId
-      ) {
-        throw Rune.invalidAction()
-      }
+    handleClick: (cellIndex, { game, playerId, allPlayerIds }) => {
+      // if (
+      //   game.cells[cellIndex].playerId !== null ||
+      //   playerId === game.lastMovePlayerId
+      // ) {
+      //   throw Rune.invalidAction()
+      // }
 
-      game.cells[cellIndex] = playerId
+      // game.cells[cellIndex] = playerId
       game.lastMovePlayerId = playerId
-      game.winCombo = findWinningCombo(game.cells)
+      console.log("allPlayerIds", allPlayerIds)
+      // game.winCombo = findWinningCombo(game.cells)
 
-      if (game.winCombo) {
-        const [player1, player2] = allPlayerIds
-
-        Rune.gameOver({
-          players: {
-            [player1]: game.lastMovePlayerId === player1 ? "WON" : "LOST",
-            [player2]: game.lastMovePlayerId === player2 ? "WON" : "LOST",
-          },
-        })
-      }
-
-      game.freeCells = game.cells.findIndex((cell) => cell === null) !== -1
-
-      if (!game.freeCells) {
-        Rune.gameOver({
-          players: {
-            [game.playerIds[0]]: "LOST",
-            [game.playerIds[1]]: "LOST",
-          },
-        })
+      // Check if the player has clicked on an empty cells
+      if (game.cells[cellIndex].playerId === null) {
+        game.cells[cellIndex].playerId = playerId
       }
     },
   },
